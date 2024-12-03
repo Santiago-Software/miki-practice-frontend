9d4748ce0577804b8bfea5dcd15fc0173820c651
EL-dev2 <el.geffen@alkerem.com>
2024-12-03 12:30:09 +0200

    Update main.ts
    deleting useless comments

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-//import { AppComponent } from './app/app.component';
 import { LayoutComponent } from './app/components/layout-area/layout/layout.component';
 
 
